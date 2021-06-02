d6afc5941903da970901ae645fe0366e99716d21
Jordan Hudgens <jordan@devcamp.com>
2021-06-02 12:52:35 -0700

    Created a starter branch

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,8 @@
 import * as React from "react";
 import Layout from "./Layout";
-import Posts from "./Posts";
 
 const App = () => {
-  return (
-    <Layout>
-      <Posts />
-    </Layout>
-  );
+  return <Layout>Coming soon...</Layout>;
 };
 
 export default App;
